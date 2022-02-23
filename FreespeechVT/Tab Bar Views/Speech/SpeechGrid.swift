f017e4d6bdfe7034a107c69102475be5597fbe3c
samueloh <samueloh@vt.edu>
2022-02-22 23:38:55 -0500

    json populated, tile padding

--- FreespeechVT/Tab Bar Views/Speech/SpeechGrid.swift
+++ FreespeechVT/Tab Bar Views/Speech/SpeechGrid.swift
@@ -13,17 +13,20 @@ struct SpeechGrid: View {
     
     @EnvironmentObject var userData: UserData
     
-    let columns = [ GridItem(.adaptive(minimum: 100), spacing: 10) ]
+    let columns = [ GridItem(.adaptive(minimum: 100), spacing: 20) ]
 
     var body: some View {
+        // add sentence preview box
         ScrollView {
             LazyVGrid(columns: columns, spacing: 3) {
                 ForEach(userData.wordsList, id: \.self) { word in
                     Text(word.name)
                         .background(Rectangle()
                                         .frame(width: 100, height: 100)
-                                        .opacity(100))
+                                        .opacity(0.3)
+                                        .foregroundColor(Color.blue))
                 }
+                .padding(.vertical, 50)
             }
         }
     }
